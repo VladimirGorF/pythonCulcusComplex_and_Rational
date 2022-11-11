1a8ee51a04d671ff827c0d04ece49abf4afd2155
Ольга <111271270+OlgaJD@users.noreply.github.com>
2022-11-11 22:18:09 +0300

    Update main.py
    Тестовый вариант. Добавлен выбор дальнейшего действия для пользователя и запуск выбранной опции

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import easygui
 import view
+
+msg = 'Выберите действие'
+title = 'Меню выбора дальнейшего действия'
+options = ['Калькулятор', 'Получить лог', 'Выход из программы']
+user_choice = easygui.buttonbox(msg, title, options)
+
+if user_choice == options[0]:  ## функция запрашивает у пользователя выражение для решения на калькуляторе, сразу убирает пробел и возвращает переменную как строку
+    example_expression = view.get_numeric_expression()
+    print(example_expression)
+elif user_choice == options[1]: ##  функция открывает пользователю лог. Не готово
+    print('log')
+elif user_choice == options[2]: ## функция вывода на экран ссообщения и выход из программы
+    view.exit()
+    quit()
+    
+    
+    
+## До исправления ===================================================================================
 
 a = input('Выберите команду от 1 до 3 ')
 
