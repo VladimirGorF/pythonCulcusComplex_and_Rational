08dd3a8bf88736bd32adf4aa63adfeb9c116118a
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-11-12 10:44:24 +0300

    Update logging.py

--- logging.py
+++ logging.py
@@ -5,3 +5,12 @@ def calcus_logger(data):                                    #Создаем фу
     time = dt.now().strftime("%d.%m.%Yг. %H:%M")            # создаем объект time в формате день, месяц, год + часы +минуты события для последующей работы с ним ниже
     with open('log.cvs', 'a') as file:                      #открываем файл "log.cvs" как объект "file"
         file.write('{}; calcus;{}\n'.format(time, data))    # записываем в наш файл построчно дату события и данные принимаемые функцией
+
+## для вызова функции логирования используйте:       
+import logging
+logging.calcus_logger(data)
+
+## для просмотра журнала логирования используйте:
+with open('log.cvs', 'r') as file:
+    for line in file:
+        print(line)
