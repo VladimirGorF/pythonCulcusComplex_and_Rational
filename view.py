8fd42cfa3f226e15b745646e7bba064b2ccff69e
Ольга <111271270+OlgaJD@users.noreply.github.com>
2022-11-15 16:42:21 +0300

    Update view.py
    Добавлена функция вывода результата расчета калькулятора пользователю

--- view.py
+++ view.py
@@ -5,20 +5,14 @@ def exit(): # Выводит на экран сообщение о выходе
 
 def get_numeric_expression(): # Запрашивает у пользователя ввод строки, убирает пробелы
     # global expression
-    user_expression = easygui.enterbox(msg="Введите выражение", title='Блок пользователя')
+    user_expression = easygui.enterbox(msg="Введите выражение", title='Калькулятор. Ввод выражения для вычисления')
     expression = user_expression.split()
     expression = ''.join(expression)
     return expression
 
-
-## До исправления ===================================================================================
-
-def view_data(data):
-    print(data)
-
-
-def get_value():
-    return input('Value = ')
-
-
-
+def show_result(expression, result): # Выводит результат расчета калькулятора пользователю
+    ex = expression
+    res = result
+    msg = f'Результат выражения {ex} :\n\n{res}'
+    title = 'Результат расчета калькулятора'
+    easygui.msgbox(msg, title)
