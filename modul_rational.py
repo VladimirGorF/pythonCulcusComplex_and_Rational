e1829811a9dc8ea7230213ba711d14184443a757
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-11-18 00:52:09 +0300

    Update modul_rational.py
    Калькулятор новый. Считает даже скобки в скобках. Применена рекурсия.

--- modul_rational.py
+++ modul_rational.py
@@ -1,9 +1,86 @@
 import re
 
+
+def Calculator(s):
+    i = 0
+    while i < len(s):  # цикл для умножения и деления
+        if s[i] == '*':
+            if s[i + 1] == '(':
+                newS = []
+                memoryI = i - 1
+                i += 2
+                while s[i] != ')':
+                    newS.append(s[i])
+                    i += 1
+                if s[memoryI - 1] == '-':
+                    s[memoryI] = - s[memoryI] * Calculator(newS)
+                    s.remove(s[memoryI - 1])
+                else:
+                    s[memoryI] = s[memoryI] * Calculator(newS)
+                i = memoryI + 1
+                while s[i] != ')':  # удаление скобкового элемента со знаком перед ним
+                    s.remove(s[i])
+                s.remove(s[i])
+            else:
+                s[i - 1] = s[i - 1] * s[i + 1]
+                s.remove(s[i])
+                s.remove(s[i])
+                i -= 1
+        elif s[i] == '/':  # деление
+            if s[i + 1] == '(':
+                newS = []
+                memoryI = i - 1
+                i += 2
+                while s[i] != ')':
+                    newS.append(s[i])
+                    i += 1
+                if s[memoryI - 1] == '-':
+                    s[memoryI] = - s[memoryI] // Calculator(newS)
+                    s.remove(s[memoryI - 1])
+                else:
+                    s[memoryI] = s[memoryI] // Calculator(newS)
+                i = memoryI + 1
+                while s[i] != ')':  # удаление скобкового элемента со знаком перед ним
+                    s.remove(s[i])
+                s.remove(s[i])
+            else:
+                s[i - 1] = s[i - 1] // s[i + 1]
+                s.remove(s[i])
+                s.remove(s[i])
+                i -= 1
+        i += 1
+    i = 0
+
+    while i < len(s):  # меняем знаки на противоположные в скобках если знак перед ней отрицательный
+        if s[i] == '(' and s[i - 1] == '-':
+            while s[i] != ')':
+                if s[i] == '-':
+                    s[i] = '+'
+                else:
+                    if type(s[i]) == int and s[i - 1] == '(':
+                        s[i] = -(s[i])
+                i += 1
+        i += 1
+    i = 0
+
+    while i < len(s):  # присвоим числам знаки
+        if s[i] == '-' and type(s[i + 1]) == int:
+            s[i + 1] = - (s[i + 1])
+            s.remove(s[i])
+            i -= 1
+        i += 1
+    res = 0
+
+    for i in range(len(s)):  # вычисляем сумму элементов списка
+        if type(s[i]) == int:
+            res += s[i]
+    return res
+
+
 def Calcus(s):
     s = re.split('', s)  # разбираем все на элементы массива
     i = 0
-    while i < len(s):
+    while i < len(s):  # цикл для обработки цифр в строке, чтобы те что рядом воспринмались, как одно число
         if s[i].isdigit() and s[i - 1].isdigit():
             s[i - 1] = int(s[i - 1]) * 10 + int(s[i])
             s[i - 1] = str(s[i - 1])
@@ -11,119 +88,84 @@ def Calcus(s):
             i -= 1
         i += 1
 
-    pos = []
-    neg = []
-    index_par = []
-    i_start = None
-    i_finish = None
+    for i in range(len(s)):  # переводим цифры в цифры
+        if s[i].isdigit():
+            s[i] = int(s[i])
 
-
-    def parenthesis():
-        i_start = None
-        i_finish = None
-        for i in range(len(s)):
-            if s[i] == '(':
-                i_start = i + 1
-                index_par.append(i_start)
-            if s[i] == ')':
-                i_finish = i
-                index_par.append(i_finish)
-                for i in range(i_start, i_finish - 1):  # cначала идем по умножению и делению
-                    if s[i] == '*':
-                        if s[i - 3] == '-':
-                            neg.append(int(s[i - 1]) * int(s[i + 1]))  # Убираем в отрицательный
-                        else:
-                            pos.append(int(s[i - 1]) * int(s[i + 1]))  # Добавляем в позитивные
-
-                for i in range(i_start, i_finish - 1):
-                    if s[i] == '/':
-                        if s[i - 3] == '-':
-                            if s[i - 2] == '-':
-                                pos.append(int(s[i - 1]) // int(s[i + 1]))  # Убираем в отрицательные
-                            else:
-                                neg.append(int(s[i - 1]) // int(s[i + 1]))  # Добавляем в позитивные
-
-                sum = 0
-                for i in range(i_start, i_finish):    # Сложение и вычитание       -(3-2)
-                    if s[i].isdigit() and s[i - 1] != '*' and s[i - 1] != '/' and s[i + 1] != '*' and s[i + 1] != '/':
-                        if s[i - 1] == '-':
-                            sum -= int(s[i])
-                        else:
-                            sum += int(s[i])
-                if s[i_start - 2] == '-':
-                    if sum > 0:
-                        neg.append(sum)
-                    else:
-                        pos.append(abs(sum))
+    i = 0
+    while i < len(s):     # цикл для умножения и деления
+        if s[i] == '*':
+            if s[i + 1] == '(':
+                newS = []
+                memoryI = i - 1
+                i += 2
+                while s[i] != ')':
+                    newS.append(s[i])
+                    i += 1
+                if s[memoryI - 1] == '-':
+                    s[memoryI] = - s[memoryI] * Calculator(newS)
+                    s.remove(s[memoryI - 1])
                 else:
-                    if sum < 0:
-                        neg.append(abs(sum))
-                    else:
-                        pos.append(sum)
+                    s[memoryI] = s[memoryI] * Calculator(newS)
+                i = memoryI + 1
+                while s[i] != ')':  # удаление скобкового элемента со знаком перед ним
+                    s.remove(s[i])
+                s.remove(s[i])
+            else:
+                s[i - 1] = s[i - 1] * s[i + 1]
+                s.remove(s[i])
+                s.remove(s[i])
+                i -= 1
+        elif s[i] == '/':    # деление
+            if s[i + 1] == '(':
+                newS = []
+                memoryI = i - 1
+                i += 2
+                while s[i] != ')':
+                    newS.append(s[i])
+                    i += 1
 
-    parenthesis()
-    index_par.append(len(s))
-    if len(index_par) < 2:
-        index_par.insert(0, 0)
-        index_par.insert(0, 0)
-    print(s)
-
-    for i in range(len(index_par)):
-        if i == 0:
-            i_start = 0
-            i_finish = index_par[i]
-        else:
-            if i % 2 == 0:
-                i_start = index_par[i - 1]
-                i_finish = index_par[i]
-
-
-            for i in range(i_start, i_finish - 1):  # cначала идем по умножению и делению
-                if s[i] == '*':
-                    if s[i + 2] == '*' or s[i + 2] == '/':
-                        if s[i + 2] == '*':
-                            if s[i - 2] == '-':
-                                neg.append(int(s[i - 1]) * int(s[i + 1]) * int(s[i + 3]))  # Убираем в отрицательный
-                            else:
-                                pos.append(int(s[i - 1]) * int(s[i + 1]) * int(s[i + 3]))   # Добавляем в позитивные
-
-                        else:
-                            if s[i - 2] == '-':
-                                neg.append(int(s[i - 1]) * int(s[i + 1]) // int(s[i + 3]))  # Убираем в отрицательный
-                            else:
-                                pos.append(int(s[i - 1]) * int(s[i + 1]) // int(s[i + 3]))   # Добавляем в позитивные
-
-                    else:
-                        if s[i - 2] != '*' and s[i - 2] != '/':
-                            if s[i - 2] == '-':
-                                neg.append(int(s[i - 1]) * int(s[i + 1]))  # Убираем в отрицательный
-                            else:
-                                pos.append(int(s[i - 1]) * int(s[i + 1]))  # Добавляем в позитивные
-
-
-
-            for i in range(i_start, i_finish - 1):
-                if s[i] == '/' and s[i - 2] != '*' and s[i - 2] != '/':
-                    if s[i - 2] == '-':
-                        neg.append(int(s[i - 1]) // int(s[i + 1]))  # Убираем в отрицательные
-                    else:
-                        pos.append(int(s[i - 1]) // int(s[i + 1]))  # Добавляем в позитивные
-
-            for i in range(i_start, i_finish - 1):  # Сложение и вычитание
-                if s[i].isdigit() and s[i - 1] != '*' and s[i - 1] != '/' and s[i + 1] != '*' and s[i + 1] != '/':
-                    if s[i - 1] == '-':
-                        neg.append(int(s[i]))
-                    else:
-                        pos.append(int(s[i]))
-            if s[i_finish - 1].isdigit() and s[i_finish - 1 - 1] != '*' and s[len(s) - 1 - 1] != '/':
-                if s[i_finish - 1 - 1] == '-':
-                    neg.append(int(s[i_finish - 1]))
+                if s[memoryI - 1] == '-':
+                    s[memoryI] = - s[memoryI] // Calculator(newS)
+                    s.remove(s[memoryI - 1])
                 else:
-                    pos.append(int(s[i_finish - 1]))
+                    s[memoryI] = s[memoryI] // Calculator(newS)
+                i = memoryI + 1
+                while s[i] != ')':    # удаление скобкового элемента со знаком перед ним
+                    s.remove(s[i])
+                s.remove(s[i])
+            else:
+                s[i - 1] = s[i - 1] // s[i + 1]
+                s.remove(s[i])
+                s.remove(s[i])
+                i -= 1
+        i += 1
+    i = 0
 
-    print(pos)
-    print(neg)
-    return sum(pos) - sum(neg)  # возвращает результат вычислений
+    while i < len(s):  # меняем знаки на противоположные в скобках если знак перед ней отрицательный
+        if s[i] == '(' and s[i - 1] == '-':
+            while s[i] != ')':
+                if s[i] == '-':
+                    s[i] = '+'
+                else:
+                    if type(s[i]) == int and s[i - 1] == '(':
+                        s[i] = -(s[i])
+                i += 1
+        i += 1
+    i = 0
+
+    while i < len(s):      # присвоим числам знаки
+        if s[i] == '-' and type(s[i + 1]) == int:
+            s[i + 1] = - (s[i + 1])
+            s.remove(s[i])
+            i -= 1
+        i += 1
+    res = 0
+
+    for i in range(len(s)):   # вычисляем сумму элементов списка
+        if type(s[i]) == int:
+            res += s[i]
+    return res
 
 
 
